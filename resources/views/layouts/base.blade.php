d82547288a74162a0533074909cb83aeea74aea9
Slobodyanik Vladimir <svvpro@gmail.com>
2015-11-25 15:09:17 +0200

    Add bower, bootstrap, base layout

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Simple site</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    {!! Html::style('bower_components/bootstrap/dist/css/bootstrap.min.css') !!}
+    {!! Html::style('css/main.css') !!}
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+@yield('content')
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+{!! Html::script('bower_components/jquery/dist/jquery.js') !!}
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+{!! Html::script('bower_components/bootstrap/dist/js/bootstrap.min.js') !!}
+</body>
+</html>
